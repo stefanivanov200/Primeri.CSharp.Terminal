71028e8176f5580aa85d9fc3d2a26c3e33569f7f
стефаниванов <kapi18@mail.bg>
2016-01-17 17:57:05 +0200

    Аритметични действия със стрингове

---
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ReadFromCommandLine
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{	//Дефиниране на променливи
+			int a = 5, b = 0;
+
+			Console.Write ("Моля въведете b = ");
+			//Въвеждане от клавиатура
+			b = Convert.ToInt32 (Console.ReadLine ());
+
+			//Печат на резултат
+			Console.WriteLine ("Резултат a + b = " +(a+b).ToString()+"\n\n");
+		}
+	}
+}
