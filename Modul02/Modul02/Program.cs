71aa98f4312c48d48945174431f328bf6c8e8806
стефаниванов <kapi18@mail.bg>
2016-01-17 12:21:26 +0200

    модул 02, оператори за целочислени действия

---
+++ Modul02/Modul02/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Modul02
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Дефиниране на променливи
+			int a = 0, b = 0;
+			int sum = 0;
+
+			// Събиране на числа
+			a = 10;
+			b = 5;
+			sum = a + b;
+
+			// Писане в конзола
+			Console.Write ( a ); Console.Write (" + "); Console.Write ( b );
+			Console.Write (" = "); 
+			Console.WriteLine ( sum );   
+			// дoпълнителни оператори
+			Console.WriteLine ("\n ползване на оператор +=");
+			a += b;                       //a=10+5, a=a+b, a=15
+			Console.WriteLine (a);
+
+			Console.WriteLine ("\n ползване на оператор -=");
+			a -= b;                       //a=15-5 a=10
+			Console.WriteLine (a);
+
+			Console.WriteLine ("\n ползване на оператор +=");
+			a *= b;                       //a=10.5, a=51
+			Console.WriteLine (a);
+
+			Console.WriteLine ("\n ползване на оператор ++");
+			a++;                      		//a=a+1. a=51
+			Console.WriteLine (a);
+
+			Console.WriteLine ("делене на цели числа" + ( 7 / 3 ).ToString ());   //целочислен резултат      
+			Console.WriteLine ("целочислен остатък" + ( 7 % 3 ).ToString ());   //целочислен остатък
+
+		}
+	}
+}
