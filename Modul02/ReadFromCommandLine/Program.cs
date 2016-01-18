6fc0aa5bc35157fc4c990fa8d2a99e93b68f4fc2
стефаниванов <kapi18@mail.bg>
2016-01-18 12:11:40 +0200

    Модул 02 Основни операции със стрингове

--- Modul02/ReadFromCommandLine/Program.cs
+++ Modul02/ReadFromCommandLine/Program.cs
@@ -10,10 +10,22 @@ namespace ReadFromCommandLine
 
 			Console.Write ("Моля въведете b = ");
 			//Въвеждане от клавиатура
-			b = Convert.ToInt32 (Console.ReadLine ());
+			//b = Convert.ToInt32 (Console.ReadLine ());
 
 			//Печат на резултат
-			Console.WriteLine ("Резултат a + b = " +(a+b).ToString()+"\n\n");
+			//Console.WriteLine ("Резултат a + b = " +(a+b).ToString()+"\n\n");
+
+			string test = " 1";
+			test += ", 2";
+			test += ", 3";
+			test += ", 4";
+			test += ", 5";
+			Console.WriteLine ("Събиране с += :" + test + "\n");
+			test = test.Replace (", ", ";");
+			Console.WriteLine ("работа с Replace:" + test + "\n");
+			Console.WriteLine ("3-ти знак: " + test.Split (';') [2]);
+
+
 		}
 	}
 }
