08c0e9f5d80f50e1c9c7985ee94335de663eed5f
стефаниванов <kapi18@mail.bg>
2016-02-07 11:44:28 +0200

    cikals s while

--- Mdul04/Mdul04/Program.cs
+++ Mdul04/Mdul04/Program.cs
@@ -6,13 +6,45 @@ namespace Mdul04
 	{
 		public static void Main (string[] args)
 		{
-			for (int i = 0; i < 5; i++)
-			{Console.WriteLine (i.ToString ());   }
-			for (int i = 5; i > 0; i--)
-			{Console.WriteLine (i.ToString ());   }
-			for (int i = 0; i <= 5; i+= 2)
-			{Console.WriteLine (i.ToString ());   }
+			string[,] _table = {
+				{ "Иван", "Георгиев", "19" },
+				{ "Васил", "Бориславов", "25" },
+				{ "Милен", "Георгиев", "38" },
+				{ "Цветан", "Иванов", "22" },
+				{ "Гергана", "Василева", "44" },
+				{ "Ивана", "Христова", "50" }
+			};
+			Console.WriteLine ("Име".PadRight(12)+"Фамилия".PadRight(12)+"Години".PadRight(12));
+			Console.WriteLine ("=====================================");
 
+//			(Пример с "do . . . while")
+			int i = 0;
+				do{Console.WriteLine (
+				_table [i, 1].PadRight (12) +
+				_table [i, 1].PadRight (12) +
+				_table [i, 2].PadRight (12));
+					i++; 	}
+			while (i < _table.Length/3);
+
+
+//			(Пример с "while")
+//			int i = 0;
+//			while (i < _table.Length / 3) 
+//			{		Console.WriteLine (
+//					_table [i, 1].PadRight (12) +
+//					_table [i, 1].PadRight (12) +
+//					_table [i, 2].PadRight (12));
+//				i++;  }
+
+//
+//			(Пример с "for")
+//			for (int i= 0; i < _table.Length/3  ; i++)
+//			{
+//			for (int j =0; j<3; j++)
+//				{
+//					Console.Write (_table [i,j].PadRight (12));
+//				}	
+//				Console.WriteLine ();	}
 		}
 	}
 }
